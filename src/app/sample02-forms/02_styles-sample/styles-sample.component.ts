9df5d9372c0251ef92be7a76f0f19b94519c2e07
x404 <kronos2003@gmail.com>
2024-05-23 17:27:21 +0300

    feat: added forms styles for input interactive

---
+++ src/app/sample02-forms/02_styles-sample/styles-sample.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-02_styles-sample',
+  templateUrl: './styles-sample.component.html',
+  styleUrls: ['./styles-sample.component.css']
+})
+export class StylesSampleComponent {
+  data: any;
+
+}
